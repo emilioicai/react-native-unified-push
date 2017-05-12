19e36bea2c1f806ebf84f016ba2e441fee10280f
Emilio Rodriguez <emiliorodriguez@gmail.com>
2017-05-12 18:03:23 +0200

    fixed multiple issues to connect with framework

--- ios/RNUnifiedPush.h
+++ ios/RNUnifiedPush.h
@@ -5,6 +5,10 @@
 #import <React/RCTBridgeModule.h>
 #endif
 
+#import <AeroGearPush/AeroGearPush-Swift.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 @interface RNUnifiedPush : NSObject <RCTBridgeModule>
 
 @end
--- ios/RNUnifiedPush.m
+++ ios/RNUnifiedPush.m
@@ -1,28 +1,26 @@
 #import "RNUnifiedPush.h"
-#import "UIKit"
-#import <AeroGearPush/AeroGearPush.h>
-#import <Foundation/Foundation.h>
 
 @implementation RNUnifiedPush
 
-@property (nonatomic) NSData * curDeviceToken;
-
-- (void)application:(UIApplication *)application
-didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    NSLog(@"APNS Success");
-    curDeviceToken = deviceToken
-}
+NSData * curDeviceToken;
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"APNS Success");
+    curDeviceToken = deviceToken;
+}
+
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(init:(NSString *)name location:(NSString *)location)
 {
-    AGDeviceRegistration *registration =
-    [[AGDeviceRegistration alloc] initWithServerURL:
+    DeviceRegistration *registration =
+    [[DeviceRegistration alloc] initWithServerURL:
      [NSURL URLWithString:@"http://push.sagaoftherealms.net/ag-push/"]];
     
     [registration registerWithClientInfo:^(id clientInfo) {
@@ -35,10 +33,10 @@ RCT_EXPORT_METHOD(init:(NSString *)name location:(NSString *)location)
         
         // --optional config--
         // set some 'useful' hardware information params
-        UIDevice *currentDevice = [UIDevice currentDevice];
-        [clientInfo setOperatingSystem:[currentDevice systemName]];
-        [clientInfo setOsVersion:[currentDevice systemVersion]];
-        [clientInfo setDeviceType: [currentDevice model]];
+//        UIDevice *currentDevice = [UIDevice currentDevice];
+//        [clientInfo setOperatingSystem:[currentDevice systemName]];
+//        [clientInfo setOsVersion:[currentDevice systemVersion]];
+//        [clientInfo setDeviceType: [currentDevice model]];
         
     } success:^() {
         NSLog(@"UPS registration worked");
@@ -50,8 +48,4 @@ RCT_EXPORT_METHOD(init:(NSString *)name location:(NSString *)location)
 }
 
 @end
-
-
-
-
 
