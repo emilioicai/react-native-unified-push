eb13527ab21c83fa0873ad9ad21b7f6c163fb79e
Emilio Rodriguez <emiliorodriguez@gmail.com>
2017-05-12 17:29:14 +0200

    using swift library for ios

--- ios/RNUnifiedPush.m
+++ ios/RNUnifiedPush.m
@@ -1,7 +1,17 @@
-
 #import "RNUnifiedPush.h"
+#import "UIKit"
+#import <AeroGearPush/AeroGearPush.h>
+#import <Foundation/Foundation.h>
 
 @implementation RNUnifiedPush
+
+@property (nonatomic) NSData * curDeviceToken;
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"APNS Success");
+    curDeviceToken = deviceToken
+}
 
 - (dispatch_queue_t)methodQueue
 {
@@ -9,5 +19,39 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(init:(NSString *)name location:(NSString *)location)
+{
+    AGDeviceRegistration *registration =
+    [[AGDeviceRegistration alloc] initWithServerURL:
+     [NSURL URLWithString:@"http://push.sagaoftherealms.net/ag-push/"]];
+    
+    [registration registerWithClientInfo:^(id clientInfo) {
+        
+        // apply the token, to identify this device
+        [clientInfo setDeviceToken:curDeviceToken];
+        
+        [clientInfo setVariantID:@"7b5243c7-7499-4b45-a4d9-a7f3e4d970aa"];
+        [clientInfo setVariantSecret:@"8a4954bb-6367-475f-8ccb-231dd2e7deb7"];
+        
+        // --optional config--
+        // set some 'useful' hardware information params
+        UIDevice *currentDevice = [UIDevice currentDevice];
+        [clientInfo setOperatingSystem:[currentDevice systemName]];
+        [clientInfo setOsVersion:[currentDevice systemVersion]];
+        [clientInfo setDeviceType: [currentDevice model]];
+        
+    } success:^() {
+        NSLog(@"UPS registration worked");
+        
+    } failure:^(NSError *error) {
+        NSLog(@"UPS registration Error: %@", error);
+    }];
+
+}
+
 @end
-  
+
+
+
+
+
